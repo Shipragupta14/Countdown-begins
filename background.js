75742f8360f45f561159f289fb5b186e3bc6989b
Shipragupta14 <shipragupta1411@gmail.com>
2017-06-03 23:23:05 +0530

    Update the runtime for updated url in the same tab of browser

--- background.js
+++ background.js
@@ -37,8 +37,9 @@ if (localStorage["time"]) {
 }
 
 var tabActive;
-chrome.tabs.onSelectionChanged.addListener(function (tabId, inf) {
-	
+
+chrome.tabs.onActivated.addListener(function(info) {	
+	console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 	//console.log(tabActive);
 	if (tabActive) {
 		var d = new Date();
@@ -47,21 +48,24 @@ chrome.tabs.onSelectionChanged.addListener(function (tabId, inf) {
 
 	}
 	 
-  console.log('onSelectionChanged', tabId, JSON.stringify(inf));
   var d = new Date();
   var start_time = d.getTime();
   var url= "";
  // console.log(tab);
-	chrome.tabs.get(tabId, function(tab) {
-
-	    url = tab.url;
+ 
+	chrome.tabs.get(info.tabId, function(tab) {
+  console.log('onActivated', info.tabId);
+  url = tab.url;
+  console.log(url);
+  
+  	
 	    if(url.indexOf("/",9)!= -1){
     	url = url.substring(0,url.indexOf("/",9))
+
     };
  
 	    tabActive = url;
 
-	    console.log(url);
 	    // adding new url in time json of the tab we jumped
 	    if(time.hasOwnProperty(url)){
 	    	time[url].start_time = start_time;
@@ -71,7 +75,12 @@ chrome.tabs.onSelectionChanged.addListener(function (tabId, inf) {
     				"total_time": 0
     			}
 	    }
+
+
+
+
 	    console.log(time);
+	    console.log("*******************");
 
 	    // Put the object into storage
 		localStorage.setItem('time', JSON.stringify(time));
@@ -79,14 +88,74 @@ chrome.tabs.onSelectionChanged.addListener(function (tabId, inf) {
 		var retrievedObject = localStorage.getItem('time');
 
 		console.log('retrievedObject: ', JSON.parse(retrievedObject));
+	
 	});
 });
 
-// chrome.tabs.onUpdated.addListener(function (tabId, inf) {
-//   console.log('onUpdated', tabId, JSON.stringify(inf));
-// });
- chrome.tabs.onReplaced.addListener(function (addedTabId, removedTabId){
- 	console.log("dfhghchcs")
- })
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+   	if(changeInfo.url){
+   		if (tabActive) {
+			var d = new Date();
+			time[tabActive].total_time  = parseInt(time[tabActive].total_time) + parseInt(d.getTime()) - parseInt(time[tabActive].start_time);
+			//time[tabActive].start_time = d.getTime(); 
+		}	
+
+		var d = new Date();
+  		var start_time = d.getTime();
+  		var url= "";
+   	    console.log(changeInfo.url);
+   		url = changeInfo.url;
+   		console.log(url);
+
+   		if(url.indexOf("/",9)!= -1){
+    		url = url.substring(0,url.indexOf("/",9))
+
+		}
+
+   		tabActive = url;
+
+	    // adding new url in time json of the tab we jumped
+	    if(time.hasOwnProperty(url)){
+	    	time[url].start_time = start_time;
+	    }else{
+    		time[url] = {
+    				"start_time"  : d.getTime(),
+    				"total_time" : 0
+    			}
+	    }
+
+	    console.log(time);
+
+   		localStorage.setItem('time', JSON.stringify(time));
+		// Retrieve the object from storage
+		var retrievedObject = localStorage.getItem('time');
+
+		console.log('retrievedObject: ', JSON.parse(retrievedObject));
+   	}
+   	console.log("##################");
+
+   });
+
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo){
+	chrome.tabs.query({currentWindow: true}, function(tabs){
+    	console.log(tabs.length);
+    	if(tabs.length == 0){
+    		if (tabActive) {
+				var d = new Date();
+				time[tabActive].total_time  = parseInt(time[tabActive].total_time) + parseInt(d.getTime()) - parseInt(time[tabActive].start_time);
+			
+			}
+			
+			localStorage.setItem('time',JSON.stringify(time));
+			var retrievedObject =  localStorage.getItem('time');
+			console.log('retrievedObject:', JSON.parse(retrievedObject));
+
+    	}
+	});
+
+
+
+});
 
 
