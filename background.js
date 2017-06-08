779ff51846d0513a482d787f1fc8634c33c1a3fa
Mukul Saini <sainimukul49@gmail.com>
2017-06-08 23:39:59 +0530

    added onfocus and onblur events

--- background.js
+++ background.js
@@ -11,6 +11,50 @@
 
 console.log("background.js");
 
+
+
+chrome.runtime.onMessage.addListener(HandleMessage);
+
+function HandleMessage(request, sender, sendResponse){
+    if (request.greeting == "onblur"){
+		if (tabActive) {
+			var d = new Date();
+			time[tabActive].total_time = parseInt(time[tabActive].total_time) + parseInt(d.getTime()) - parseInt(time[tabActive].start_time);
+			time[tabActive].start_time = d.getTime();
+		}
+		localStorage.setItem('time',JSON.stringify(time));
+		var retrievedObject =  localStorage.getItem('time');
+        sendResponse({farewell: "goodbye"});        
+    }else if(request.greeting == "onfocus"){
+    	chrome.tabs.query({currentWindow: true, active: true}, function(tab){
+			        var d = new Date();
+			  		var start_time = d.getTime();
+			        url = tab[0].url;
+
+			        if(url.indexOf("/",9)!= -1){
+			    		url = url.substring(0,url.indexOf("/",9))
+			    	}
+				    tabActive = url;
+
+				    if(time.hasOwnProperty(url)){
+				    	time[url].start_time = start_time;
+				    }else{
+			    		time[url] = {
+			    				"start_time"  : start_time,
+			    				"total_time": 0
+			    			}
+				    }
+					localStorage.setItem('time', JSON.stringify(time));
+					var retrievedObject = localStorage.getItem('time');
+					
+		});
+		sendResponse({farewell: "welcome"});
+    }
+};
+
+
+
+
 /*
 chrome.tabs.onActivated.addListener(function(info) {
 	///console.log(new Date());
