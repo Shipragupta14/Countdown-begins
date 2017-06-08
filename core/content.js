d3758361608fb1086c34d733548cb7bff0c570e8
Shipragupta14 <shipragupta1411@gmail.com>
2017-06-09 01:59:17 +0530

    Added onfocus and onblur event

---
+++ core/content.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+window.onblur =function(){
+	chrome.runtime.sendMessage({greeting: "onblur"}, function(response) {
+	    console.log(response.farewell);
+	});
+}
+window.onfocus =function(){
+	chrome.runtime.sendMessage({greeting: "onfocus"}, function(response) {
+	    console.log(response.farewell);
+	});
+}
